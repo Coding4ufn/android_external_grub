4245f5d490f703a17047c7fd14b2e89d8173a087
David Keppler <dkeppler@mitre.org>
2014-06-10 10:38:58 -0400

    Update grub menu for SVMP image layout changes
    The system disk layout was changed which shifted the partition ordering
    around a little. This updates the grub preset menu to match where the
    boot image is now.
    
    Change-Id: Ie56f20e9b21b7cc611b618a2878367944827da39

--- stage2/preset_menu.c
+++ stage2/preset_menu.c
@@ -1,16 +1,11 @@
 const char *preset_menu =
     "rootnoverify (hd0,0)\n"
-    "default=1\n"
+    "default=0\n"
     "hiddenmenu\n"
     "timeout 0\n"
-    "fallback 1 0\n"
+    "fallback 0\n"
     "\n"
-    "title Android Install to /dev/sda from /dev/sdb\n"
-    "       cmdline (hd1,0)/cmdline\n"
-    "       kernel --use-cmd-line (hd1,0)/kernel\n"
-    "       initrd (hd1,0)/ramdisk\n"
-    "\n"
-    "title Android Startup from /dev/sda\n"
-    "       cmdline (hd0,2)/cmdline\n"
-    "       kernel --use-cmd-line (hd0,2)/kernel\n"
-    "       initrd (hd0,2)/ramdisk\n";
+    "title SVMP Startup from /dev/sda\n"
+    "       cmdline (hd0,1)/cmdline\n"
+    "       kernel --use-cmd-line (hd0,1)/kernel\n"
+    "       initrd (hd0,1)/ramdisk\n";
